57c9e01c7f3035b18116b1c9fd8acd7d3589914c
Kevin Sweeney <sweeney.kj@gmail.com>
2015-02-27 14:33:47 +0800

    list page modifications for viewing any user or no user

--- client/app/items/list/list.controller.js
+++ client/app/items/list/list.controller.js
@@ -3,29 +3,37 @@
 angular.module('swapApp')
   .controller('ItemsCtrl', function ($scope, $http, $stateParams, Auth) {
 
-    console.log('ItemsCtrl');
-    _.forIn($stateParams, function(value, key){
-      console.log('param: ' + key + ' | ' + value);
-    });
+    //console.log('ItemsCtrl');
+    //_.forIn($stateParams, function(value, key){
+    //  console.log('param: ' + key + ' | ' + value);
+    //});
 
     $scope.articles = [];
-    $scope.user = {};
+    $scope.user = null;
+    $scope.isMe = false;
     
     if($stateParams['userId'] && $stateParams['userId'] != 'me'){
       $scope.user = $stateParams['userId'];
     }
-    else{
+    else if(Auth.isLoggedIn() && $stateParams['userId'] == 'me'){
+      $scope.isMe = true;
       $scope.user = Auth.getCurrentUser()._id.toString();
     }
 
-    $http.get('/api/articles?uid='+$scope.user).success(function(articles) {
-      console.log('articles found: ' + articles.length);
+    var promise = $scope.user == null ?
+                                $http.get('/api/articles'):
+                                $http.get('/api/articles?uid='+$scope.user);
+    promise.success(function(articles) {
+      //console.log('articles found: ' + articles.length);
       $scope.articles = articles;
     });
 
     $scope.ownsArticle = function(article){
       //console.log('  article owner: ' + article.owner.toString());
       //console.log('auth user owner: ' + Auth.getCurrentUser()._id.toString());
+      if(!Auth.isLoggedIn()){
+        return false;
+      }
       return Auth.getCurrentUser()._id.toString() == article.owner.toString();
     };
 
