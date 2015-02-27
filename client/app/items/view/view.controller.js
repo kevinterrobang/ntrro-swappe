72c1d831bfb7d4ac3b02ad0cb9bff5264259d70c
Kevin Sweeney <sweeney.kj@gmail.com>
2015-02-27 13:02:55 +0800

    setting up view page

--- client/app/items/view/view.controller.js
+++ client/app/items/view/view.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('swapApp')
-	.controller('ViewItemCtrl', function ($scope, $http){
+	.controller('ViewItemCtrl', function ($scope, $http, $stateParams, Auth){
 		console.log('ViewItemCtrl');
+
+		if(!$stateParams.articleId){
+			console.log('no article ID. Redirect to... my list?');
+			$location.path('/items');
+		}
+
+		$scope.article = {};
+
+		$http.get('/api/articles/' + $stateParams.articleId).success(function(article){
+			$scope.article = article;
+		});
 	});
