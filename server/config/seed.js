8ff9668c957c361f22985948715bbe7406de0ee7
Kevin Sweeney <sweeney.kj@gmail.com>
2015-02-27 16:19:37 +0800

    modifying test data: removing things and adding articles with owners.

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,30 +5,8 @@
 
 'use strict';
 
-var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
-
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-  });
-});
+var Article = require('../api/article/article.model');
 
 User.find({}).remove(function() {
   User.create({
@@ -42,8 +20,38 @@ User.find({}).remove(function() {
     name: 'Admin',
     email: 'admin@admin.com',
     password: 'admin'
-  }, function() {
+  }, function(err, testUser, adminUser) {
       console.log('finished populating users');
+      createArticles(testUser, adminUser);
     }
   );
 });
+
+var createArticles = function(testUser, adminUser){
+  Article.find({}).remove(function(){
+
+    Article.create({
+      name: 'Orange Blouse',
+      info: 'This is a very nice orange blouse.',
+      owner: testUser._id,
+    }, {
+      name: 'Red Blouse',
+      info: 'This is a very nice red blouse.',
+      owner: testUser._id,
+    }, {
+      name: 'Purple Blouse',
+      info: 'This is a very nice purple blouse.',
+      owner: testUser._id,
+    }, {
+      name: 'Purple Blouse',
+      info: 'This is a very nice purple blouse.',
+      owner: adminUser._id,
+    }, {
+      name: 'Green Blouse',
+      info: 'This is a very nice green blouse.',
+      owner: adminUser._id,
+    }, function(){
+      console.log('finished populating articles');
+    });
+  });
+};
